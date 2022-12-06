e936b18cdd7b1f9252b8f02d289bdc1e5bdfb31a
Ani <aavetisian@noorlogic.com>
2022-12-06 23:22:16 +0400

    Fixed button

--- Travel/index.js
+++ Travel/index.js
@@ -1,1 +1,10 @@
-
+const encrypt = (str) => {
+    let result = '';
+    for (let i = 0; i < str.length; i += 2) {
+      const nextSymbol = str[i + 1] || '';
+      result = `${result}${nextSymbol}${str[i]}`;
+    }
+  
+    return result;
+  };
+  console.log(encrypt('kovalenko'))
